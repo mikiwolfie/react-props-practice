40f5f55b0d2f9b63446c1df1f6da36a88892bb3d
unknown <samanthamikhail24@gmail.com>
2021-02-08 14:28:07 +0800

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card";
+import contacts from "../contacts";
 
+function createCard(contact) {
+  return <Card name={contact.name}
+  img={contact.imgURL}
+  tel={contact.phone}
+  email={contact.email}/>;
+}
 
 function App() {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <Card />
-    </div>
+      <h1 className="heading">My Contacts</h1>,
+      {contacts.map(createCard)}
+
+      // <Card
+      // name={contacts[0].name}
+      // imgURL={contacts[0].imgURL}
+      // tel={contacts[0].tel}
+      // email={contacts[0].email}
+      // />
+      // <Card
+      // name={contacts[1].name}
+      // imgURL={contacts[1].imgURL}
+      // tel={contacts[1].tel}
+      // email={contacts[1].email}
+      // />
+      // <Card
+      // name={contacts[2].name}
+      // imgURL={contacts[2].imgURL}
+      // tel={contacts[2].tel}
+      // email={contacts[2].email}
+      // />
+
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,24 @@
 import React from "react";
+import Avatar from "./Avatar"
+import Detail from "./detail"
 
 function Card(props) {
+  return (
   <div className="card">
     <div className="top">
-      <h2 className="name">{pros.name}</h2>
-      <img className="circle-img"
-        src={pros.img}
-        alt="avatar_img"
-      />
+      <h2 className="name">{props.name}</h2>
+      <Avatar imgURL={props.imgURL}/>
     </div>
     <div className="bottom">
-      <p className="info">{pros.tel}</p>
-      <p className="info">{pros.email}</p>
+    <Detail
+    detailInfo={props.tel}
+    />
+    <Detail
+    detailInfo={props.email}
+    />
     </div>
   </div>
+);
 }
 
-ReactDOM.render (
-  <Card
-    name="Violet Evergarden"
-    img="https://i.pinimg.com/originals/23/05/5a/23055aecf5abdc1fc686b62c3087243a.jpg"
-    tel="+123 456 789"
-    email="VioletEvergarden@letters.com"
-  />
-
-  <Card
-    name="Hinata Shoyo"
-    img="https://i.pinimg.com/originals/39/8f/7a/398f7a78603616ff2e6f320e39505531.png"
-    tel="+987 654 321"
-    email="hinataShoyo@volleyball.com"
-  />
-
-  <Card
-    name="Mitsuba Sousuke"
-    img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvUFVPiI2oWuyOnyCtYkL-y75zUsiD7CGcTg&usqp=CAU"
-    tel="+918 372 574"
-    email="Mitsuba@camera.com"
-  />
-)
+export default Card;
